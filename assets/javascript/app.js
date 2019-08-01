b12a36593c68715aca001e440976a21bc8e2ab05
RBaird2001 <rbaird2001@yahoo.com>
2019-08-01 08:32:17 -0600

    scoreboard badges functional and sample questions increased

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,48 @@
 var seconds = 1000;
 var currQuestion = 0;
 var timerQuestion ;
-var correct=0 ;
-var incorrect=0 ;
+var countCorrect=0 ;
+var countIncorrect=0 ;
 
 var arrQuestions = [
     {
-        question: "I am a: ",played: false,answers: [
-            {answer:"fat old slob.",correct: true},
-            {answer:"jelly donut.",correct:false},
-            {answer:"a disease and you're the cure.",correct:false},
-            {answer:"loser baby, so why don't you kill me.",correct:false},
+        question: "How many staircases are there at Hogwarts",played: false,answers: [
+            {answer:"142",correct: true},
+            {answer:"734",correct:false},
+            {answer:"286",correct:false},
+            {answer:"88",correct:false},
+        ]
+    },
+    {
+        question: "What need of Dumbledore's was fulfilled by the Room of Requirement",played: false,answers: [
+            {answer:"Need to urinate.",correct: true},
+            {answer:"Need to people from  Voldemort.",correct:false},
+            {answer:"Need to train students secretly.",correct:false},
+            {answer:"Need to store the Pensive. ",correct:false},
+        ]
+    },
+    {
+        question: "Which character responded to being fired by getting intoxicated",played: false,answers: [
+            {answer:"Winky.",correct: true},
+            {answer:"Hagrid.",correct:false},
+            {answer:"Cornelius Fudge.",correct:false},
+            {answer:"Vernon Dursley.",correct:false},
+        ]
+    },
+    {
+        question: "What do Harry Potter and Neville Longbottom have in common",played: false,answers: [
+            {answer:"They share the same birthday.",correct: true},
+            {answer:"Both of their parents were killed when they were young.",correct:false},
+            {answer:"They are the same height.",correct:false},
+            {answer:"Both were marked for death by Voldemort.",correct:false},
+        ]
+    },
+    {
+        question: "Which of the following is NOT true about a Patronous",played: false,answers: [
+            {answer:"The animal it produces determines its strength.",correct: true},
+            {answer:"They can be used to communicate.",correct:false},
+            {answer:"Producing one is very difficult.",correct:false},
+            {answer:"They can change to a different animal in certain circumstances.",correct:false},
         ]
     },
 ]
@@ -52,7 +84,8 @@ function generateResponse(boolCorrect){
 function correct(correctAnswer){ 
     $("#question").addClass("bg-success").html("<h2>CORRECT!</h2>")
     correctAnswer.addClass("bg-success");
-    correct++
+    countCorrect++
+    $("#correctAnswers").html(countCorrect)
     setTimeout(initQuestion,7*seconds);
 }
 
@@ -60,13 +93,15 @@ function incorrect(incorrectAnswer){
     $("#question").addClass("bg-danger").html("<h2>Incorrect.</h2>");
     incorrectAnswer.addClass("bg-danger")
     $("li").filter("[data-correct=true]").addClass("bg-success");
-    incorrect++
+    countIncorrect++;
+    $("#incorrectAnswers").html(countIncorrect);
     setTimeout(initQuestion,7*seconds);
 }
 
 function noAnswer(){
     $("#question").addClass("bg-danger").html("<h2>No answer.</h2>");
     $("li").filter("[data-correct=true]").addClass("bg-success");
-    incorrect++
+    countIncorrect++;
+    $("#incorrectAnswers").html(countIncorrect);
     setTimeout(initQuestion,7*seconds);
 }
